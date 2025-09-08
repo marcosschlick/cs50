bfb042af6dad43fc2ce2cedcb53ffad50fa5d809
Marcos Schlick <marcosschlick@gmail.com>
2025-09-07 23:04:56 -0300

    add week2 readability project

---
+++ week2/readability/readability.c
@@ -0,0 +1,76 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
+int count_sentences(string text);
+int count_words(string text);
+
+int main(void)
+{
+    string text = get_string("Text: ");
+    int letters = count_letters(text);
+    int sentences = count_sentences(text);
+    int words = count_words(text);
+
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
+
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+    int grade = index;
+
+    if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
+    }
+}
+
+int count_letters(string text)
+{
+    int letters = 0;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+    }
+    return letters;
+}
+
+int count_sentences(string text)
+{
+    int sentences = 0;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
+    return sentences;
+}
+
+int count_words(string text)
+{
+    int words = 1;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isspace(text[i]))
+        {
+            words++;
+        }
+    }
+    return words;
+}
+
