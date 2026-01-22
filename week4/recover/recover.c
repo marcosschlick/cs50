cd35ce687150dd097512dfd92de9cdb8c46b7e77
Marcos Schlick <marcosschlick@gmail.com>
2026-01-22 09:12:29 -0300

    add week4 recover project

---
+++ week4/recover/recover.c
@@ -0,0 +1,51 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    // Accept a single command-line argument
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
+    // Open the memory card
+    FILE *card = fopen(argv[1], "r");
+
+    // Create a buffer for a block of data
+    uint8_t buffer[512];
+
+    FILE *jpg = NULL;
+    char filename[8];
+    int n_images = 0;
+
+    // While there's still data left to read from the memory card
+    while (fread(buffer, 1, 512, card) == 512)
+    {
+        // Create JPEGs from the data
+        if ((buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff) &&
+            ((buffer[3] & 0xf0) == 0xe0))
+        {
+            if (jpg != NULL)
+            {
+                fclose(jpg);
+            }
+
+            sprintf(filename, "%03d.jpg", n_images);
+
+            jpg = fopen(filename, "w");
+            n_images++;
+        }
+        if (jpg != NULL)
+        {
+            fwrite(buffer, 1, 512, jpg);
+        }
+    }
+
+    // Close files
+    fclose(card);
+    fclose(jpg);
+}
+
